a165fc0ef8a91f70cf6775ac919749541cb28c34
AlesMaver <ales.maver@gmail.com>
2020-06-26 23:41:15 +0200

    Merging changes

--- makeXLSXoutputs.R
+++ makeXLSXoutputs.R
@@ -1,12 +1,18 @@
 #!/usr/bin/env Rscript
 library("optparse")
 library("openxlsx")
-
 options(stringsAsFactors = F)
 
+# Parse input options
 option_list = list(
   make_option(c("--sample_basename"), type="character", default=NULL, help="Exome id, for example PX5000", metavar="character"),
   make_option(c("--RARE_FUNCTIONAL"), type="character", default=NULL, help="RARE_FUNCTIONAL output", metavar="character"),
+  make_option(c("--HET_DOMINANT"), type="character", default=NULL, help="HET_DOMINANT output", metavar="character"),
+  make_option(c("--COMPHET_RECESSIVE"), type="character", default=NULL, help="COMPHET_RECESSIVE output", metavar="character"),
+  make_option(c("--HOM_RECESSIVE"), type="character", default=NULL, help="HOM_RECESSIVE output", metavar="character"),
+  make_option(c("--CLINVAR_PATHOGENIC"), type="character", default=NULL, help="CLINVAR_PATHOGENIC output", metavar="character"),
+  make_option(c("--CLINVAR_FILTERED"), type="character", default=NULL, help="CLINVAR_FILTERED output", metavar="character"),
+  make_option(c("--CLINVAR_ALL"), type="character", default=NULL, help="CLINVAR_ALL output", metavar="character"),
   make_option(c("--XLSX_OUTPUT"), type="character", default=NULL, help="Output XLSX", metavar="character")
 );
 opt_parser = OptionParser(option_list=option_list);
@@ -17,9 +23,17 @@ if (is.null(opt$RARE_FUNCTIONAL)){
   stop("At least one argument must be supplied (input file).n", call.=FALSE)
 }
 
+# Make a list of reports
 reportList<-list()
 reportList$RARE_FUNCTIONAL <- read.table(opt$RARE_FUNCTIONAL, sep="\t", header=T, quote="", dec = ".", fill=NA)
+reportList$HET_DOMINANT <- read.table(opt$HET_DOMINANT, sep="\t", header=T, quote="", dec = ".", fill=NA)
+reportList$COMPHET_RECESSIVE <- read.table(opt$COMPHET_RECESSIVE, sep="\t", header=T, quote="", dec = ".", fill=NA)
+reportList$HOM_RECESSIVE <- read.table(opt$HOM_RECESSIVE, sep="\t", header=T, quote="", dec = ".", fill=NA)
+reportList$CLINVAR_PATHOGENIC <- read.table(opt$CLINVAR_PATHOGENIC, sep="\t", header=T, quote="", dec = ".", fill=NA)
+reportList$CLINVAR_FILTERED <- read.table(opt$CLINVAR_FILTERED, sep="\t", header=T, quote="", dec = ".", fill=NA)
+reportList$CLINVAR_ALL <- read.table(opt$CLINVAR_ALL, sep="\t", header=T, quote="", dec = ".", fill=NA)
 
+# Prepare outputs
 hs <- createStyle(fontColour = "#ffffff", fgFill = "#4F80BD",
                   halign = "center", valign = "center", textDecoration = "Bold",
                   border = "TopBottomLeftRight", textRotation = 45)
