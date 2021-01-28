ac14fd7c7f415ed065cd0d36dcadfb2fce485393
AlesMaver <ales.maver@gmail.com>
2021-01-28 23:46:02 +0100

    Fixed error where T nucleotide was converted to TRUE

--- makeXLSXoutputs.R
+++ makeXLSXoutputs.R
@@ -37,20 +37,20 @@ if (is.null(opt$RARE_FUNCTIONAL) &
   stop("At least one input must be supplied. Exiting with error...", call.=FALSE)
 }
 
-# #Debugging code - import outputs from a directory
-# opt <- list()
-# SAMPLE="SAMPLE"
-# snpSift_outputDir<-"/home/ales/pipeline_debug/"
-# opt$RARE_FUNCTIONAL<-paste0(snpSift_outputDir, SAMPLE, ".RARE_FUNCTIONAL.tab")
-# opt$HET_DOMINANT<-paste0(snpSift_outputDir, SAMPLE, ".HET_DOMINANT.tab")
-# opt$COMPHET_RECESSIVE<-paste0(snpSift_outputDir, SAMPLE, ".COMPHET_RECESSIVE.tab")
-# opt$HOM_RECESSIVE<-paste0(snpSift_outputDir, SAMPLE, ".HOM_RECESSIVE.tab")
-# opt$CLINVAR_PATHOGENIC<-paste0(snpSift_outputDir, SAMPLE, ".CLINVAR_PATHOGENIC.tab")
-# opt$CLINVAR_FILTERED<-paste0(snpSift_outputDir, SAMPLE, ".CLINVAR_FILTERED.tab")
-# opt$CLINVAR_ALL<-paste0(snpSift_outputDir, SAMPLE, ".CLINVAR_ALL.tab")
-# opt$PANEL_FILTERED<-paste0(snpSift_outputDir, SAMPLE, ".PANEL_FILTERED.tab")
-# opt$PANEL_ALL<-paste0(snpSift_outputDir, SAMPLE, ".PANEL_ALL.tab")
-# opt$MITOMAP<-paste0(snpSift_outputDir, SAMPLE, ".MITOMAP.tab")
+#Debugging code - import outputs from a directory
+opt <- list()
+SAMPLE="PX5816"
+snpSift_outputDir<-"/home/ales/pipeline_debug/"
+opt$RARE_FUNCTIONAL<-paste0(snpSift_outputDir, SAMPLE, ".RARE_FUNCTIONAL.tab")
+opt$HET_DOMINANT<-paste0(snpSift_outputDir, SAMPLE, ".HET_DOMINANT.tab")
+opt$COMPHET_RECESSIVE<-paste0(snpSift_outputDir, SAMPLE, ".COMPHET_RECESSIVE.tab")
+opt$HOM_RECESSIVE<-paste0(snpSift_outputDir, SAMPLE, ".HOM_RECESSIVE.tab")
+opt$CLINVAR_PATHOGENIC<-paste0(snpSift_outputDir, SAMPLE, ".CLINVAR_PATHOGENIC.tab")
+opt$CLINVAR_FILTERED<-paste0(snpSift_outputDir, SAMPLE, ".CLINVAR_FILTERED.tab")
+opt$CLINVAR_ALL<-paste0(snpSift_outputDir, SAMPLE, ".CLINVAR_ALL.tab")
+opt$PANEL_FILTERED<-paste0(snpSift_outputDir, SAMPLE, ".PANEL_FILTERED.tab")
+opt$PANEL_ALL<-paste0(snpSift_outputDir, SAMPLE, ".PANEL_ALL.tab")
+opt$MITOMAP<-paste0(snpSift_outputDir, SAMPLE, ".MITOMAP.tab")
 
 
 # Make a list of reports
@@ -65,6 +65,18 @@ if( !is.null(opt$CLINVAR_ALL) & file.exists(opt$CLINVAR_ALL) ) reportList$CLINVA
 if( !is.null(opt$PANEL_FILTERED) ) if ( file.exists(opt$PANEL_FILTERED) ) reportList$PANEL_FILTERED <- read.table(opt$PANEL_FILTERED, sep="\t", header=T, quote="", dec = ".", fill=NA, colClasses = c("character"))
 if( !is.null(opt$PANEL_ALL) ) if ( file.exists(opt$PANEL_ALL) ) reportList$PANEL_ALL <- read.table(opt$PANEL_ALL, sep="\t", header=T, quote="", dec = ".", fill=NA, colClasses = c("character"))
 if( !is.null(opt$MITOMAP) ) if ( file.exists(opt$MITOMAP) ) reportList$MITOMAP <- read.table(opt$MITOMAP, sep="\t", header=T, quote="", dec = ".", fill=NA, colClasses = c("character"))
+
+# Convert logical columns to single letters - this is to disable T letters being read as logicals
+convertLogicalsBackToCharacters <- function(df){
+  df[,sapply(df,class) == "logical"] <- sapply(df[,sapply(df,class) == "logical"],
+                                               function(i) substr(as.character(i),1,1))
+  return(df)
+}
+
+for(sheetName in names(reportList)){
+  if(nrow(reportList[[sheetName]])==0) next
+  reportList[[sheetName]] <- convertLogicalsBackToCharacters(reportList[[sheetName]])
+}
 
 # Find compound heterozygous candidates
 if("COMPHET_RECESSIVE" %in% names(reportList)){
