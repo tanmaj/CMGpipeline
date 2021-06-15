e84af16959395528784d11e4f13dcc2ce475d803
AlesMaver <ales.maver@gmail.com>
2021-06-15 13:02:05 +0200

    Make mitomap.py tolerant to Mitomap not updating certificate

--- mitomap/mitomap.py
+++ mitomap/mitomap.py
@@ -4,7 +4,7 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 try:
-    response = requests.post("https://mitomap.org/mitomaster/websrvc.cgi", files={"file": open("mtDNA.fasta"),'fileType': ('', 'sequences'),'output':('', 'detail')})
+    response = requests.post("https://mitomap.org/mitomaster/websrvc.cgi", files={"file": open("mtDNA.fasta"),'fileType': ('', 'sequences'),'output':('', 'detail')}, verify=False)
     print(str(response.content, 'utf-8'))
 except requests.exceptions.HTTPError as err:
     print("HTTP error: " + err)
